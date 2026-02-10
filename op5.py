3476c8a678cc1a7dc905b0d7648916494aeadd54
Karina <karinaqxw@gmail.com>
2026-02-10 10:46:33 +0200

    add task

---
+++ op5.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def display_info(self):
+        print(self.name, self.price, self.quantity)
